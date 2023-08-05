a12aaee7e0ef609314cea41aa1ad9ff6a5345fb6
criatura435 <127697449+criatura435@users.noreply.github.com>
2023-08-05 10:02:22 -0500

    api routes created

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+// require controller functionality
+
+
+// api/thoughts CRUD operations for all thoughts
+// router.route('/')
+//     .get()
+//     .post();
+
+// api/thoughts/:thoughtId CRUD operations for single thought
+// router.route('/:thoughtId')
+//     .get()
+//     .post()
+//     .put()
+//     .delete();
+
+module.exports = router;
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+// require controller functionality
+
+
+// api/users CRUD operations for all users
+// router.route('/')
+//     .get()
+//     .post();
+
+// api/users/:userId CRUD operations for single user
+// router.route('/:userId')
+//     .get()
+//     .post()
+//     .put()
+//     .delete();
+
+module.exports = router;
